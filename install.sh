f0fea589915a0183a0b32012263ec2838d8db954
Eric Johnson <erjohnso@google.com>
2014-02-25 23:18:47 +0000

    remove separate css and install mod_headers

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # This script assumes that Debian-7 wheezy Compute Engine images are used
-# and that the user has already installed the apache2 package
 #
 # Prior to using this install script, please execute
 #   $ sudo apt-get update && sudo apt-get upgrade -y
 #   $ sudo apt-get install git apache2 -y
+#sudo DEBIAN_FRONTEND=noninteractive apt-get update
+#sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -y >/dev/null 2>&1
 
 pushd `dirname $0` > /dev/null
 MY_PATH=`pwd`
@@ -13,12 +14,12 @@ popd > /dev/null
 
 NAME=$(hostname -s)
 
-echo "=> Copying HTML files, apache2.conf, and restarting service..."
-sudo cp $MY_PATH/demo.css /var/www
+echo "=> Copying HTML files, apache2.conf, enable mod-header, and restarting"
 sudo cp $MY_PATH/index.html /var/www
 sudo sed -i "s|@MY_INSTANCE_NAME@|$NAME|" /var/www/index.html
-sudo chmod 644 /var/www/*
+sudo chmod 644 /var/www/index.html
 sudo cp $MY_PATH/apache2.conf /etc/apache2/apache2.conf
+sudo ln -s /etc/apache2/mods-available/headers.load /etc/apache2/mods-enabled/headers.load
 sudo service apache2 restart
 
 exit 0
